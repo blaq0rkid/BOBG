122f2f623c1af1a0fa8f1a79e887c89080a1cab4
blaq0rkid <blaq0rkid@users.noreply.github.com>
2026-04-01 10:24:32 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
+cat > vite.config.js 'EOF'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // or '/repo-name/' for GitHub Pages
+  base: '/BOBG/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
   }
 })
+EOF
